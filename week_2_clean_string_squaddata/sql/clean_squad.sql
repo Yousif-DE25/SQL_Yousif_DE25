53feb5a5b64dfcbb3cdf17f4f400a3e42a02bffa
Yousif Alsaedi <yousifalsaedi90@yahoo.com>
2025-11-21 14:01:00 +0100

    week 2 clean

--- week_2_clean_string_squaddata/sql/clean_squad.sql
+++ week_2_clean_string_squaddata/sql/clean_squad.sql
@@ -8,7 +8,47 @@ SELECT
 FROM staging.squad
 WHERE NOT regexp_matches(context, title); -- two arguments are string and substring
 
-/* task three*/
+/* task three */
 
 -- show rows if context starting with title
 
+-- use LIKE operator with wildcard%
+SELECT * FROM staging.squad
+WHERE context LIKE CONCAT(title,'%'); 
+-- check results, the results not okey, because wildcard _
+
+-- regular expression functions will make underscore be a literal character
+SELECT * FROM staging.squad
+WHERE regexp_matches(context, CONCAT('^', title));
+
+/* task four */
+
+-- show a new column, frist answer from AI model
+
+SELECT
+  answers[18:], -- slicing
+  answers[19], -- indexing
+  CASE
+    WHEN answers[19] = ' ' THEN NULL
+    ELSE answers[18:]
+  END AS striped_answer,
+  INSTR(striped_answer, '''') AS first_quotiation_index, 
+  -- singel quation ''' needs to be '''' to work
+  striped_answer[:first_quotiation_index-1] AS first_answer,
+--  striped_answer[:6],
+  answers
+FROM staging.squad;
+
+/* task five */
+
+-- generate same resualt as four, with patern matching
+
+SELECT 
+  -- don't allow singel ' 
+  regexp_extract(answers, '''([^'']+)'',') AS first_answer,
+  -- allow upper and lower case, digit, space, comma
+  regexp_extract(answers, '''([A-Za-z0-9 ,]+)'',') AS first_answer_1,
+  -- use the grouping argument in regexp_extract 
+  regexp_extract(answers, '''([A-Za-z0-9 ,]+)'',',1) AS first_answer_2,
+  answers
+FROM staging.squad;
--- week_2_crud_aggregate_hrdata/sql/query_employee.sql
+++ week_2_crud_aggregate_hrdata/sql/query_employee.sql
@@ -1,21 +1,73 @@
-DESC;
+/* ============
+   DDL - CREATE
+   ============ */
+-- create schema
+CREATE SCHEMA IF NOT EXISTS staging; 
 
-DESC staging.employees;
+-- create sequence to generate values for employee_id column later
+CREATE SEQUENCE IF NOT EXISTS id_sequence START 1; 
 
-SELECT * FROM staging.employees;
+-- create table
+CREATE TABLE IF NOT EXISTS staging.employees (
+    employee_id INTEGER DEFAULT nextval('id_sequence'),
+    department VARCHAR,
+    employment_year INTEGER
+); 
 
-SELECT
-    department,
-    employment_year
-FROM staging.employees;
-WHERE employee_id = '6'
+/* =============
+   CRUD - CREATE 
+   ============= */
+-- insert 3 rows manually
+INSERT INTO
+    staging.employees (department, employment_year)
+VALUES
+    ('Sales', 2001),
+    ('Logistics', 2002),
+    ('IT', 2002);
 
-DESC;
+-- insert with read_csv() function
+-- ref: https://duckdb.org/docs/stable/guides/file_formats/csv_import
+INSERT INTO
+    staging.employees (department, employment_year)
+    SELECT * FROM read_csv('data/employees.csv');
 
-DESC staging.more_employees;
+/* =============
+   CRUD - Read 
+   ============= */
+SELECT * FROM staging.employees; 
+--LIMIT 10;
+--OFFSET 10; 
 
-SELECT * FROM staging.more_employees;
+/* ==============
+   CRUD - Update 
+   ============== */
+-- modify existing data
+UPDATE staging.employees
+SET employment_year = 2023
+WHERE employee_id IN (98,99);
+--WHERE employee_id = 98 OR employee_id = 99; 
 
-SELECT
-    employee_id
-FROM staging.more_employees;
+/* ============
+   DDL - Alter
+   ============ */
+ALTER TABLE staging.employees
+ADD COLUMN pension_plan VARCHAR DEFAULT 'plan 1'
+
+/* ==============
+   CRUD - Update 
+   ============== */
+UPDATE staging.employees
+SET pension_plan = 'plan 2'
+WHERE employment_year > 2015; 
+
+/* ==============
+   CRUD - Delete 
+   ============== */
+-- always check the rows you plan to delete first
+SELECT *
+FROM staging.employees
+WHERE employee_id = 1; 
+
+DELETE
+FROM staging.employees
+WHERE employee_id = 1; 
